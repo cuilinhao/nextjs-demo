0cf7ea212ea44483b12727dc893b66a0293a6fad
cuilinhao <15021538370@163.com>
2025-06-17 18:41:53 +0800

    测试平行路由，使用default.tsx解决找不到路由问题

--- app/default.tsx
+++ app/default.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Page() {
   return (
        <h1 className='flex mt-6 p-6 bg-gray-500 text-white rounded-lg'>
-        @children
+        @children default
         </h1>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,16 +20,30 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  team,
+  analytics
 }: Readonly<{
   children: React.ReactNode;
+  team: React.ReactNode;
+  analytics: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-    
+        <div className="container mx-auto">
+          <div className="flex justify-center text-blue-500 p-6 gap-6">
+            <Link href="/">Home</Link>
+            <Link href="/visitors">Visitors</Link>
+          </div>
+          <div className="flex gap-6">
+            {team}
+            {analytics}
+          </div>
+
         {children}
+        </div>
       </body>
     </html>
   );
